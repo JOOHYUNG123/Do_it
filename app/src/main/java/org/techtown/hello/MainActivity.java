6d3b2c462f72c336a5cabf132e198b0670a166a4
LEE <jooh9992@gmail.com>
2020-06-04 09:46:09 +0900

    이제 시작2

--- app/src/main/java/org/techtown/hello/MainActivity.java
+++ app/src/main/java/org/techtown/hello/MainActivity.java
@@ -1,8 +1,12 @@
 package org.techtown.hello;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +15,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(this, "화인1 버튼을 눌렀어요.", Toast.LENGTH_LONG).show();
+   }
+   public void onButton2Clicked(View v){
+       Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+       startActivity(myIntent);
+   }
+   public void onButton3Clicked(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
+   }
 }
